58a7a91a4155d365eae0643e5dc7656d53fd2690
Isaac Avilez <isaac1180@aol.com>
2019-12-10 21:35:08 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,10 +1,7 @@
 # Make your shoe class here!
 class Shoe 
+  attr_accessor 
   def initialize(brand, color, size, material, condition)
     @brand = brand
-    # @color = color 
-    # @size = size 
-    # @material = material
-    # @condition = condition
   end
 end 
